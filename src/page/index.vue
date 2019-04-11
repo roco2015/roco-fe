ddba0751c7369b100de8b9b4333f672b64cb0469
dongjiawen <dongjiawen@kanzhun.com>
2019-04-11 21:46:53 +0800

    简易首页2

--- src/page/index.vue
+++ src/page/index.vue
@@ -1,13 +1,7 @@
 <template>
     <div id="index">
-      <div class="banner"></div>
       <main class="main">
-        <br/>
-        <p>who are you?</p>
-        <p>怎么进来的?</p>
-        <p>主页?</p>
-        <p>没做呢</p>
-        <p>先用这个凑活吧</p>
+        首页
       </main>
     </div>
 </template>
@@ -20,29 +14,16 @@ export default {
 
 <style scoped>
 #index {
-  position: relative;
+  display: flex;
+  flex-flow: column;
   height: 100vh;
-  background: url("../assets/images/bg/bg-2.png");
-}
-#index::before {
-  content: '';
-  position: absolute;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  background-color: white;
-  opacity: 0.2;
-}
-.banner {
-  width: 1000px;
-  height: 400px;
-  margin: 0 auto;
-  background: url("../assets/images/bg/bg-3.jpg") no-repeat center top / 100% auto;
+  background: url("../assets/images/bg/bg-1.png");
 }
 .main {
-  width: 1000px;
-  margin: 0 auto;
-  font-size: 20px;
+  flex: 1;
+  line-height: 100vh;
+  color: white;
+  font-size: 60px;
+  text-align: center;
 }
 </style>
